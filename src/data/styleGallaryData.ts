2f4ba9bd331d87c8d1178ef5cc75187ec6187d4b
Ananya Nair <ananya.nair2907@gmail.com>
2025-08-04 07:48:52 +0000

    Add styleGallaryData.ts

---
+++ src/data/styleGallaryData.ts
@@ -0,0 +1,60 @@
+
+interface StyleInfo {
+    name: string;
+    hint: string;
+    imageUrl: string;
+  }
+  
+  export const ALL_STYLES: StyleInfo[] = [
+    {
+      name: 'Art Deco',
+      hint: 'art deco building',
+      imageUrl: 'https://firebasestorage.googleapis.com/v0/b/styleblend.firebasestorage.app/o/style-images%2Fartdeco.png?alt=media'
+    },
+    {
+      name: 'Cartoon',
+      hint: 'cartoon character',
+      imageUrl: 'https://firebasestorage.googleapis.com/v0/b/styleblend.firebasestorage.app/o/style-images%2Fcartoon.png?alt=media&token=cf010aa1-7bcd-4884-b185-a93b6df7d153'
+    },
+    {
+      name: 'Cubism',
+      hint: 'cubist portrait',
+      imageUrl: 'https://firebasestorage.googleapis.com/v0/b/styleblend.firebasestorage.app/o/style-images%2Fcubism.png?alt=media&token=cb85b900-f903-4588-974b-f08f54db9540'
+    },
+    {
+      name: 'Cyberpunk',
+      hint: 'cyberpunk city',
+      imageUrl: 'https://firebasestorage.googleapis.com/v0/b/styleblend.firebasestorage.app/o/style-images%2Fcyberpunk.png?alt=media&token=5e8c1e6c-7b72-4c7a-9f04-cca60cc3c180'
+    },
+    {
+      name: 'Expressionism',
+      hint: 'expressionist painting',
+      imageUrl: 'https://firebasestorage.googleapis.com/v0/b/styleblend.firebasestorage.app/o/style-images%2Fexpressionism.png?alt=media&token=60f908fc-b317-480b-8463-7aa1a35f9669'
+    },
+    {
+      name: 'Impressionism',
+      hint: 'impressionist painting',
+      imageUrl: 'https://firebasestorage.googleapis.com/v0/b/styleblend.firebasestorage.app/o/style-images%2Fimpressionism.png?alt=media&token=1ca9acd6-a824-465d-9297-8a203261ac1b'
+    },
+    {
+      name: 'Pop Art',
+      hint: 'pop art comic',
+      imageUrl: 'https://firebasestorage.googleapis.com/v0/b/styleblend.firebasestorage.app/o/style-images%2Fpop_art.png?alt=media&token=5ab5883a-88aa-4326-bcb2-786a31aafe42'
+    },
+    {
+      name: 'Renaissance',
+      hint: 'renaissance painting',
+      imageUrl: 'https://firebasestorage.googleapis.com/v0/b/styleblend.firebasestorage.app/o/style-images%2Frenaissance.png?alt=media&token=91b80bb7-4cc5-445b-b76d-c99871bc7bc5'
+    },
+    {
+      name: 'Steampunk',
+      hint: 'steampunk machine',
+      imageUrl: 'https://firebasestorage.googleapis.com/v0/b/styleblend.firebasestorage.app/o/style-images%2Fsteampunk.png?alt=media&token=952a4218-0332-4bd4-bbc0-1b9b91774efd'
+    },
+    {
+      name: 'Swedish Form',
+      hint: 'swedish furniture design',
+      imageUrl: 'https://firebasestorage.googleapis.com/v0/b/styleblend.firebasestorage.app/o/style-images%2Fswedishform.png?alt=media&token=4d2f765f-d84a-4d29-8415-05c68baa6ee2'
+    }
+  ];
+  
